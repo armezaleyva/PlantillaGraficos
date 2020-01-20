cdd1051d99ea85322e06646ec4a62d37eadd39a9
Armando Meza Leyva <armezaleyva@gmail.com>
2020-01-20 07:34:11 -0700

    Initial commit

---
+++ PlantillaGraficos/PlantillaGraficos.cpp
@@ -0,0 +1,60 @@
+// PlantillaGraficos.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define GLEW_STATIC
+
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// Declarar una ventana
+	GLFWwindow* window;
+
+	// Si no se pudo iniciar GLFW
+	// Terminamos ejecución
+	if (!glfwInit()) {
+		exit(EXIT_FAILURE);
+	}
+
+	// Si se pudo iniciar GLFW
+	// Inicializamos la ventana
+	window = glfwCreateWindow(800, 600, "Ventana", NULL, NULL);
+
+	// Si no se pudo crear la ventana
+	// Terminamos la ejecución
+	if (!window) {
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+
+	// Establecemos la ventana como contexto
+	glfwMakeContextCurrent(window);
+
+	glewExperimental = true;
+
+	// Ciclo de dibujo (Draw loop)
+	while (!glfwWindowShouldClose(window)) {
+		// Establecer región de dibujo
+		glViewport(0, 0, 800, 600);
+		// Establecemos el color de borrado
+		glClearColor(1, 0.2, 0.5, 1);
+		// Borrar!
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// Actualizar valores y dibujar
+
+		glfwSwapBuffers(window);
+	}
+	// Después del ciclo de dibujo
+	// Eliminamos ventana y procesos de glfwr
+	glfwDestroyWindow(window);
+	glfwTerminate();
+}
